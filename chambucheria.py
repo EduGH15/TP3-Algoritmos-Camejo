17b63f7e678cef4b04853599d1c4b9a45b418e3b
EduGH15 <eduardogl15.personal@gmail.com>
2023-06-09 16:50:35 -0300

    He agregado la funcion de modificar, hasta ahora funciona correctamente

--- chambucheria.py
+++ chambucheria.py
@@ -3,22 +3,35 @@ import csv
 import os
 
 RESERVA = "reservas.csv"
+
 POSICiON_COMANDO = 1
 POSICION_ID = 2
+
 POSICION_NOMBRE = 3
 POSICION_CANTIDAD_PERSONA = 4
 POSICION_TIEMPO = 5
 POSICION_UBICACION = 6
+
 COMANDO_AGREGAR = "agregar"
 COMANDO_MODIFICAR = "modificar"
 COMANDO_ELIMINAR = "eliminar"
+
+def asignar_posicion(campo):
+    if campo == "nombre":
+        return 1
+    elif campo == "cant_personas":
+        return 2
+    elif campo == "tiempo":
+        return 3
+    elif campo == "ubicacion":
+        return 4
 
 def nuevo_id(nombre_archivo):
     archivo = open(nombre_archivo)
     lector = csv.reader(archivo, delimiter=";")
     reservas = list(lector)
     id = reservas[len(reservas) - 1][0]
-    return int(id) + 1
+    return str(int(id) + 1)
     
 def agregar_reserva(argv, nombre_archivo):
     id = nuevo_id(nombre_archivo)
@@ -66,9 +79,14 @@ def eliminar_reserva(argv, nombre_archivo):
     
     os.rename("datos_auxiliares.csv", nombre_archivo)
 
-"""
 def modificar_reserva(argv, nuevos_datos, nombre_archivo):
-    
+    id = argv[POSICION_ID]
+    lista_nuevos_datos = nuevos_datos.split()
+    campo = lista_nuevos_datos[0]
+    valor_campo = lista_nuevos_datos[1]
+    #posicion_campo_fila = 0
+    posicion_campo_columna = asignar_posicion(campo)
+    nueva_linea = []
     try:
         archivo = open(nombre_archivo)
     except:
@@ -76,24 +94,39 @@ def modificar_reserva(argv, nuevos_datos, nombre_archivo):
         return
     
     try:
-        archivo_auxiliar = open("datos_auxiliares.txt", "a")
+        archivo_auxiliar = open("datos_auxiliares.csv", "a")
     except:
         archivo.close()
         print("error al abrir el archivo auxiliar")
         return
 
-"""
+    lector = csv.reader(archivo, delimiter=";")
+    escritor = csv.writer(archivo_auxiliar, delimiter=";")
+    reservas = list(lector)
+
+    for i in range(len(reservas)):
+        nueva_linea = reservas[i].copy()
+        for j in range(len(reservas[i])):
+            if reservas[i][0] == id:
+                nueva_linea[posicion_campo_columna] = valor_campo
+
+        escritor.writerow(nueva_linea)
+
+    archivo.close()
+    archivo_auxiliar.close()
     
+    os.rename("datos_auxiliares.csv", nombre_archivo)
+            
 
 def main(): 
     if sys.argv[POSICiON_COMANDO] == COMANDO_AGREGAR:
         agregar_reserva(sys.argv, RESERVA)
 
-    """
+
     if sys.argv[POSICiON_COMANDO] == COMANDO_MODIFICAR and sys.argv[POSICION_ID].isnumeric():
         datos = input("¿Qué desea cambiar?:")
         modificar_reserva(sys.argv, datos, RESERVA)
-    """
+
 
     if sys.argv[POSICiON_COMANDO] == COMANDO_ELIMINAR and sys.argv[POSICION_ID].isnumeric():
         eliminar_reserva(sys.argv, RESERVA)
