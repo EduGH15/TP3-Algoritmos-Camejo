86cd6ab177b4d3f27211c546daaae3378d5e779f
EduGH15 <eduardogl15.personal@gmail.com>
2023-06-11 15:38:42 -0300

    Se realizaron cambios en el nombre de sys.argv y se cambiaron algunas ctes

--- chambucheria.py
+++ chambucheria.py
@@ -7,10 +7,10 @@ RESERVA = "reservas.csv"
 POSICiON_COMANDO = 1
 POSICION_ID = 2
 
-POSICION_NOMBRE = 3
-POSICION_CANTIDAD_PERSONA = 4
-POSICION_TIEMPO = 5
-POSICION_UBICACION = 6
+POSICION_NOMBRE = 2
+POSICION_CANTIDAD_PERSONAS = 3
+POSICION_HORARIO = 4
+POSICION_UBICACION = 5
 
 COMANDO_AGREGAR = "agregar"
 COMANDO_MODIFICAR = "modificar"
@@ -19,7 +19,7 @@ COMANDO_LISTAR = "listar"
 
 CAMPO_ID = "id"
 CAMPO_NOMBRE = "nombre"
-CAMPO_CANTIDAD_PERSONAS = "cant_personas"
+CAMPO_CANTIDAD_PERSONAS = "cant"
 CAMPO_HORARIO = "horario"
 CAMPO_UBICACION = "ubicacion"
 
@@ -45,38 +45,63 @@ def es_horario_valido(horario):
             es_valido = False
         elif i != 2 and not lista_horario[i].isnumeric():
             es_valido = False
+        elif i == 0 and int(horario[i])>2:
+            es_valido = False
+        elif i == 0 and int(horario[i]) == 2 and int(horario[i + 1]) > 3:
+            es_valido = False
+        elif i == 3 and int(horario[i]) > 5:
+            es_valido = False
         i+= 1
 
     return es_valido   
 
+def es_ubicacion_valida(ubicacion):
+    return ubicacion == "F" or ubicacion == "D"
 
-def imprimir_error_modificar(nombre_archivo):
-    if len(sys.argv) != 3:
+def es_nombre_valido(nombre):
+    return nombre.isalpha()
+
+def es_cantidad_valida(cantidad):
+    return (not cantidad.isalpha()) and int(cantidad) > 0
+
+def imprimir_error_agregar(argumentos):
+    if len(argumentos) != 6:
+        print("La cantidad de argumentos ingresado es invalida. Ingrese: <archivo> agrega nombre cant_personas HH:MM ubicacion")
+    elif not es_nombre_valido(argumentos[POSICION_NOMBRE]):
+        print("el nombre ingresado no es válido. Ingrese un nombre sin números.")
+    elif not es_cantidad_valida(argumentos[POSICION_CANTIDAD_PERSONAS]):
+        print("La cantidad de personas ingresadas es inválida. Ingrese un número mayor a cero.")
+    elif not es_horario_valido(argumentos[POSICION_HORARIO]):
+        print("El horario es inválido. Ingrese un horario entre las 00:00 y las 23:59.")
+    elif not es_ubicacion_valida(argumentos[POSICION_UBICACION]):
+        print("La ubicación es inválida. Ingrese D o F.")
+
+def imprimir_error_modificar(argumentos, nombre_archivo):
+    if len(argumentos) != 3:
         print("El nro de argumentos ingresados es incorrecto. ingrese: <archivo> modificar id.")
-    elif not(sys.argv[POSICION_ID].isnumeric()):
+    elif not(argumentos[POSICION_ID].isnumeric()):
         print("El id debe ser un número.")
-    elif not existe_id(sys.argv[POSICION_ID], nombre_archivo):
+    elif not existe_id(argumentos[POSICION_ID], nombre_archivo):
         print("No se puede modificar esta reserva porque no existe.")
 
-def imprimir_error_eliminar(nombre_archivo):
-    if len(sys.argv) != 3:
+def imprimir_error_eliminar(argumentos, nombre_archivo):
+    if len(argumentos) != 3:
         print("El nro de argumentos ingresados es incorrecto. ingrese: <archivo> eliminar id.")
-    elif not(sys.argv[POSICION_ID].isnumeric()):
+    elif not(argumentos[POSICION_ID].isnumeric()):
         print("El id debe ser un número.")
-    elif not existe_id(sys.argv[POSICION_ID], nombre_archivo):
+    elif not existe_id(argumentos[POSICION_ID], nombre_archivo):
         print("No se puede eliminar esta reserva porque no existe.")
 
-def imprimir_error_listar(nombre_archivo):
-    if len(sys.argv) == 3:
+
+
+def imprimir_error_listar(argumentos, nombre_archivo):
+    if len(argumentos) == 3:
         print("El número de argumentos no es válido. Ingrese: <archivo> listar id_inicial id_final o <archivo> listar")
-    elif not existe_id(sys.argv[2], nombre_archivo) or not existe_id(sys.argv[3], nombre_archivo):
+    elif not existe_id(argumentos[2], nombre_archivo) or not existe_id(argumentos[3], nombre_archivo):
         print("No se puede listar el archivo porque uno de los dos id no existe")
-    elif sys.argv[2] < sys.argv[3]:
+    elif argumentos[2] < argumentos[3]:
         print("El primer id debe ser mayor al segundo")
 
-def imprimir_error_agregar(nombre_archivo):
-    if len(sys.argv) != 6:
-        print("La cantidad de argumentos ingresado es invalida. Ingrese: <archivo> agrega nombre cant_personas HH:MM ubicacion")
 
 def asignar_nuevo_formato(campo):
     if campo == CAMPO_ID:
@@ -108,14 +133,10 @@ def nuevo_id(nombre_archivo):
     return str(int(id) + 1)
     archivo.close()
     
-def agregar_reserva(argv, nombre_archivo):
+def agregar_reserva(argumentos, nombre_archivo):
     id = nuevo_id(nombre_archivo)
-    nombre = argv[2]
-    cantidad_personas = argv[3]
-    tiempo = argv[4]
-    ubicacion = argv[5]
-    nueva_linea = [id, nombre, cantidad_personas, tiempo, ubicacion]
-
+    nueva_linea = [id, argumentos[POSICION_NOMBRE], argumentos[POSICION_CANTIDAD_PERSONAS], argumentos[POSICION_HORARIO], argumentos[POSICION_UBICACION]]
+    
     try:
         archivo = open(nombre_archivo, "a")
     except:
@@ -126,8 +147,8 @@ def agregar_reserva(argv, nombre_archivo):
     escritor.writerow(nueva_linea)
     archivo.close()
 
-def eliminar_reserva(argv, nombre_archivo):
-    id = argv[POSICION_ID]
+def eliminar_reserva(argumentos, nombre_archivo):
+    id = argumentos[POSICION_ID]
 
     try:
         archivo = open(nombre_archivo)
@@ -154,11 +175,11 @@ def eliminar_reserva(argv, nombre_archivo):
     
     os.rename("datos_auxiliares.csv", nombre_archivo)
 
-def modificar_reserva(argv, nuevos_datos, nombre_archivo):
-    id = argv[POSICION_ID]
-    lista_nuevos_datos = nuevos_datos.split()
-    campo = lista_nuevos_datos[0]
-    valor_campo = lista_nuevos_datos[1]
+def modificar_reserva(argumentos, lista_datos, nombre_archivo):
+    id = argumentos[POSICION_ID]
+    #lista_nuevos_datos = nuevos_datos.split()
+    campo = lista_datos[0]
+    valor_campo = lista_datos[1]
     posicion_campo_columna = asignar_posicion(campo)
     nueva_linea = []
     try:
@@ -191,7 +212,7 @@ def modificar_reserva(argv, nuevos_datos, nombre_archivo):
     
     os.rename("datos_auxiliares.csv", nombre_archivo)
 
-def listar_reserva(argv, nombre_archivo):      
+def listar_reserva(argumentos, nombre_archivo):      
     try:
         archivo = open(nombre_archivo)
     except:
@@ -225,33 +246,40 @@ def listar_reserva(argv, nombre_archivo):
 
     archivo.close()
 
-def main(): 
-    if sys.argv[POSICiON_COMANDO] == COMANDO_AGREGAR and len(sys.argv) == 6 and es_horario_valido(sys.argv[4]):
-        agregar_reserva(sys.argv, RESERVA)
+def main():
+    argumentos = sys.argv.copy()
+     
+    if argumentos[POSICiON_COMANDO] == COMANDO_AGREGAR and len(argumentos) == 6 and es_nombre_valido(argumentos[POSICION_NOMBRE]) and es_cantidad_valida(argumentos[POSICION_CANTIDAD_PERSONAS]) and es_horario_valido(argumentos[POSICION_HORARIO]) and es_ubicacion_valida(argumentos[POSICION_UBICACION]):
+        agregar_reserva(argumentos, RESERVA)
         print("Se agregó con exito")
-    elif sys.argv[POSICiON_COMANDO] == COMANDO_AGREGAR:
-        imprimir_error_agregar(RESERVA)
-
-    if sys.argv[POSICiON_COMANDO] == COMANDO_MODIFICAR and len(sys.argv) == 3 and sys.argv[POSICION_ID].isnumeric() and existe_id(sys.argv[POSICION_ID], RESERVA):
-        datos = input("¿Qué desea cambiar?:")
-        modificar_reserva(sys.argv, datos, RESERVA)
-        print("Modificación exitosa.")
-    elif sys.argv[POSICiON_COMANDO] == COMANDO_MODIFICAR:
-        imprimir_error_modificar(RESERVA)
-        return
-
-    if sys.argv[POSICiON_COMANDO] == COMANDO_ELIMINAR and len(sys.argv) == 3 and sys.argv[POSICION_ID].isnumeric() and existe_id(sys.argv[POSICION_ID], RESERVA):
-        eliminar_reserva(sys.argv, RESERVA)
-        print("Se eliminó con éxito")
-    elif sys.argv[POSICiON_COMANDO] == COMANDO_ELIMINAR:
-        imprimir_error_eliminar(RESERVA)
+    elif argumentos[POSICiON_COMANDO] == COMANDO_AGREGAR:
+        imprimir_error_agregar(argumentos)
         return
     
-    if (sys.argv[POSICiON_COMANDO] == COMANDO_LISTAR and len(sys.argv) == 2) or (sys.argv[POSICiON_COMANDO] == COMANDO_LISTAR and len(sys.argv) == 4 and (existe_id(sys.argv[2], RESERVA) and existe_id(sys.argv[3], RESERVA)) and (sys.argv[2] > sys.argv[3])):
-        listar_reserva(sys.orig_argv, RESERVA)
-        print("Se enlistó con éxito")
-    elif sys.argv[POSICiON_COMANDO] == COMANDO_LISTAR:
+    if argumentos[POSICiON_COMANDO] == COMANDO_MODIFICAR and len(argumentos) == 3 and argumentos[POSICION_ID].isnumeric() and existe_id(argumentos[POSICION_ID], RESERVA):
+        datos = input("¿Qué desea cambiar?:")
+        lista_datos = datos.split()
+        if len(lista_datos) == 2 and ((lista_datos[0] == CAMPO_NOMBRE and es_nombre_valido(lista_datos[1])) or (lista_datos[0] == CAMPO_CANTIDAD_PERSONAS and es_cantidad_valida(lista_datos[1])) or (lista_datos[0] == CAMPO_HORARIO and es_horario_valido(lista_datos[1])) or (lista_datos[0] == CAMPO_UBICACION and es_ubicacion_valida(lista_datos[1]))):
+            modificar_reserva(sys.argv, lista_datos, RESERVA)
+            print("Modificación exitosa.")
+    elif sys.argv[POSICiON_COMANDO] == COMANDO_MODIFICAR:
+        imprimir_error_modificar(argumentos, RESERVA)
+        return
+    
+    if argumentos[POSICiON_COMANDO] == COMANDO_ELIMINAR and len(argumentos) == 3 and argumentos[POSICION_ID].isnumeric() and existe_id(argumentos[POSICION_ID], RESERVA):
+        eliminar_reserva(argumentos, RESERVA)
+        print("Se eliminó con éxito")
+    elif argumentos[POSICiON_COMANDO] == COMANDO_ELIMINAR:
+        imprimir_error_eliminar(argumentos, RESERVA)
+        return
+    
+    """
+    if (argumentos[POSICiON_COMANDO] == COMANDO_LISTAR and len(argumentos) == 2) or (argumentos[POSICiON_COMANDO] == COMANDO_LISTAR and len(argumentos) == 4 and (existe_id(argumentos[2], RESERVA) and existe_id(argumentos[3], RESERVA)) and (argumentos[2] > argumentos[3])):
+        listar_reserva(argumentos, RESERVA)
+    elif argumentos[POSICiON_COMANDO] == COMANDO_LISTAR:
         imprimir_error_listar(RESERVA)
         return
+    """
+
 if __name__ == "__main__":
     main()
